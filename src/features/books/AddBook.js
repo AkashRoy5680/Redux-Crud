8255c1285f12afc3466d0e5ab6aa4c137f5adc14
AkashRoy5680 <royakash1272@gmail.com>
2022-08-02 02:20:25 +0200

    books show from state

--- src/features/books/AddBook.js
+++ src/features/books/AddBook.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddBook = () => {
+    const {title,setTitle}=useState("");
+    const {author,setAuthor}=useState("");
     return (
         <div>
             <h2>Add Book</h2>
+            <form>
+                <div className='form-field'>
+                    <label htmlFor='title'>Title: </label>
+                    <input type="text" 
+                    name="title" 
+                    id="title" 
+                    value={title} 
+                    onChange={(e)=>setTitle(e.target.value)} 
+                    required />
+                </div>
+                <div className='form-field'>
+                    <label htmlFor='author'>Author: </label>
+                    <input type="text" 
+                    name="author" 
+                    id="author" 
+                    value={author} 
+                    onChange={(e)=>setAuthor(e.target.value)} 
+                    required />
+                </div>
+                <div>
+                    <button type="submit">Add Book</button>
+                </div>
+            </form>
         </div>
     );
 };
